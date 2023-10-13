e49d746f308882456a29900c73b1728819e158a4
Эмиль Чалаев <chalaevemil@yandex.ru>
2023-10-13 22:26:54 +0300

    frontend diploma v1.3.1

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,13 +4,10 @@ import "./NotFound.css";
 export default function NotFound() {
   return (
     <div className="not-found">
-      <div>
-        <h2 className="not-found__error">404</h2>
-        <p className="not-found__message">Страница не найдена</p>
-      </div>
-      <NavLink to="/" className="not-found__link">
-        Назад
-      </NavLink>
+      <h2 className="not-found__error">404</h2>
+      <p className="not-found__message">Страница не найдена</p>
+      <NavLink to="/" className="not-found__link">Назад</NavLink>
     </div>
   );
 }
+  
